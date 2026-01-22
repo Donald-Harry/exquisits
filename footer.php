4d622d50b1e63177bf22cf3229ff3cca2c24b89a
Bright Munemu <brightmunemu@gmail.com>
2026-01-22 23:08:16 +0100

    add livechat

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
+<?php
+include "livechat.php"
+    ?>
 <footer class="bg-dark">
     <div class="container">
         <div class="row">
             <div class="f-items default-padding">
                 <div class="col-md-3 col-sm-4 logo">
-                     <a href="#"><img src="assets/img/logo.png" alt="Logo" style="width:8vw; height:15vh;" /></a> 
-                     <!--<h2>Exquisit</h2>-->
+                    <a href="#"><img src="assets/img/logo.png" alt="Logo" style="width:8vw; height:15vh;" /></a>
+                    <!--<h2>Exquisit</h2>-->
                 </div>
 
                 <!-- Single Item -->
@@ -125,16 +128,16 @@
     </div>
     <!-- End Footer Bottom -->
     <!-- Smartsupp Live Chat script -->
-<!-- Smartsupp Live Chat script -->
-<!--<script type="text/javascript">-->
-<!--var _smartsupp = _smartsupp || {};-->
-<!--_smartsupp.key = 'af9af4d54d32f31c119f9a2fae6ae0579bae012e';-->
-<!--window.smartsupp||(function(d) {-->
-<!--  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];-->
-<!--  s=d.getElementsByTagName('script')[0];c=d.createElement('script');-->
-<!--  c.type='text/javascript';c.charset='utf-8';c.async=true;-->
-<!--  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);-->
-<!--})(document);-->
-<!--</script>-->
-<!--<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>-->
+    <!-- Smartsupp Live Chat script -->
+    <!--<script type="text/javascript">-->
+    <!--var _smartsupp = _smartsupp || {};-->
+    <!--_smartsupp.key = 'af9af4d54d32f31c119f9a2fae6ae0579bae012e';-->
+    <!--window.smartsupp||(function(d) {-->
+    <!--  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];-->
+    <!--  s=d.getElementsByTagName('script')[0];c=d.createElement('script');-->
+    <!--  c.type='text/javascript';c.charset='utf-8';c.async=true;-->
+    <!--  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);-->
+    <!--})(document);-->
+    <!--</script>-->
+    <!--<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>-->
 </footer>
