5971ad4faa113b322743884b3885940c11180f9b
Bright Munemu <brightmunemu@gmail.com>
2026-09-04 08:14:49 +0100

    add

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
                                     <i class="fas fa-map-marker-alt"></i>
                                 </div>
                                 <div class="info">
-                                    <span>Addressssss</span> 9413 Highfield Road
+                                    <span>Addresses</span> 9413 Highfield Road
                                     BOURNEMOUTH
                                     BH28 8RC
                                 </div>
